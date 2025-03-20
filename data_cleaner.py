59a76ff6f6f6c82412304eeece95d0797a0e4575
School account <schoolaccount@Samsons-MacBook-Pro-2.local>
2025-03-20 19:47:56 -0400

    added file for data cleaner

---
+++ data_cleaner.py
@@ -0,0 +1,9 @@
+'''
+authors:        Samson Chow 
+date created:   March 20, 2025
+purpose:        The purpose of this script is to aggregate the data by 
+                team and then pull out useful stats and clean the data in 
+                preparation for training
+'''
+
+
