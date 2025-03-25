af56f4e25817a7f97bfc6a6c35089cb23b82caf2
School account <schoolaccount@Samsons-MacBook-Pro-2.local>
2025-03-25 12:16:11 -0400

    added code for getting dates from games

--- data_cleaner.py
+++ data_cleaner.py
@@ -36,6 +36,44 @@ def extract_team_name_from_path(path):
     
     return None
 
+# function that returns YYMMDD from filepath
+def get_date_from_path(path: str) -> str:
+
+    # clean up the path first, we know it will always be in the format of
+    # game33_date201414.CSV (because i coded it to be like that)
+    path = path.split('.CSV')[0]
+    date = path.split('date')[1]
+
+    # this is where i screwed up and made things hard initially:
+    # i did not add 0's in the file name to keep the formatting consistent
+    # so january shouldve been 01 but its 1 which makes things a lot harder
+    year = date[2:4]
+    date = date[4:]
+
+    # now we have 4 cases: 
+    # 1. MD, 2. MMD, 3. MDD, 4. MMDD
+
+    if len(date) == 2:
+        # we know it must be MD
+        month = '0' + date[0]
+        day = '0' + date[1]
+    elif len(date) == 3:
+        # since there is no possible way to tell the difference between MMD and MDD
+        # for example 123 (jan 23) and (dec 3) I will implement the following workaround:
+        # there are more months / total months that have one digit when compared to
+        # days / total days with one digit, so we will just always assume its MDD
+        month = '0' + date[0]
+        day = date[1:]
+    elif len(date) == 4:
+        # just MMDD
+        month = date[:2]
+        day = date[2:]
+
+    res = year + month + day
+    assert(len(res) == 6), 'date length not 6'
+
+    return res
+    
 # funciton for cleaning a spreadsheet
 def clean_data(file_path: str, team: Team) -> pd.DataFrame:
     '''
@@ -119,6 +157,11 @@ def clean_data(file_path: str, team: Team) -> pd.DataFrame:
     data.at[0, 'team'] = team_id
     data.at[0, 'opponent'] = opp_id
 
+    # append date
+    date = get_date_from_path(path)
+    assert(len(date) == 6), f'{date} not correct'
+    data['date'] = date
+
     # can comment out: add the date of the game
     # data['date'] = file_path[-11:-3]
 
@@ -161,7 +204,7 @@ if __name__ == '__main__':
                 except Exception as e:
                     print(f"Error processing {path}: {e}")
     
-    res.to_csv('cleaned_data.CSV', index=False)
+    res.to_csv('cleaned_data_with_dates.CSV', index=False)
 
                 
 
